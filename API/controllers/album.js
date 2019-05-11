06d5b036e8aad62b7cb0234eb481df939965880c
RumblingBee <m.dezette@gmail.com>
2019-05-11 16:29:29 +0200

    Ajout méthode pour récupérer le catalogue

--- API/controllers/album.js
+++ API/controllers/album.js
@@ -2,9 +2,32 @@ var mongoose = require('mongoose'),
 album = mongoose.model('Album');
 
 exports.list_all_albums = function(req, res) {
+
+  var offset = parseInt(req.params.offset);
+  var limit =  parseInt(req.params.limit);
+
     album.find({}, function(err, album) {
       if (err)
         res.send(err);
       res.json(album);
-    });
+    }).limit(limit).sort({annee: 1}).skip(offset);
+
+    res.setHeader('Access-Control-Allow-Origin',"*");
+  };
+
+
+
+  
+exports.get_catalog = function(req, res) {
+
+  var offset = parseInt(req.params.offset);
+  var limit =  parseInt(req.params.sort);
+
+    album.find({}, function(err, album) {
+      if (err)
+        res.send(err);
+      res.json(album);
+    }).limit(4).sort({prix: 1, artiste:1}).skip(offset);
+
+    res.setHeader('Access-Control-Allow-Origin',"*");
   };
--- API/routes/album.js
+++ API/routes/album.js
@@ -5,7 +5,10 @@ module.exports = function(app){
 const Album = require('../controllers/album');
 
 
- app.route('/albums')
+ app.route('/albums/:offset/:limit')
     .get(Album.list_all_albums);
+
+ app.route('/catalog/:offset/:limit')
+    .get(Album.get_catalog);
 
 }
