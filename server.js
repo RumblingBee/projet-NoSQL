092f110a4493b19b9f8c7b4701a8bd7630fe681d
RumblingBee <m.dezette@gmail.com>
2019-05-11 12:16:05 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+
+  mongoose = require('mongoose'),
+  Album = require('./api/models/album'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/music'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/album'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+console.log('Serveur démarré sur le port ' + port);
